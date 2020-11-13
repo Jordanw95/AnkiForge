913209b80258eb5320036d1c7e53ef1487c4d57a
Jordanw95 <65023872+Jordanw95@users.noreply.github.com>
2020-11-13 12:20:39 +0100

    Setting enviroment

--- AnkiForge/env/bin/activate.fish
+++ AnkiForge/env/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jordanwaters/Desktop/Python/GitHubRepositories/AnkiForge/env"
+set -gx VIRTUAL_ENV "/Users/jordanwaters/Desktop/Python/GitHubRepositories/AnkiForge/AnkiForge/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
--- AnkiForge/env/bin/django-admin.py
+++ AnkiForge/env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/Users/jordanwaters/Desktop/Python/GitHubRepositories/AnkiForge/env/bin/python3.8
+#!/Users/jordanwaters/Desktop/Python/GitHubRepositories/AnkiForge/AnkiForge/env/bin/python3.8
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
