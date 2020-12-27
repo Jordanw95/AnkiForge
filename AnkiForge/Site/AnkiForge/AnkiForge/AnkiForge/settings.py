d31e6649d3974d26015ed9763eaf44aadc724c1c
Jordanw95 <65023872+Jordanw95@users.noreply.github.com>
2020-12-27 16:30:10 +0100

    Static serving
    - Set up amazon S3 to serve staitc files
    - Configured templates so that they now run bootstraps css and js from static files (either in s3 cloud front or local static in container)

--- AnkiForge/Site/AnkiForge/AnkiForge/AnkiForge/settings.py
+++ AnkiForge/Site/AnkiForge/AnkiForge/AnkiForge/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from SecretKeys.secretsettings import *
+from SecretKeys.amazonkeys import *
 from celery.schedules import crontab
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -187,7 +188,29 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_URL = '/static/'
+# STATIC_URL = '/static/'
+# STATICFILES_DIRS = [
+#    os.path.join(BASE_DIR, "static"),
+#    ] 
+
+""" Need to collect static when ready for production and this will pass everything over th
+to s3 cloud front, also need to change settgins for allowed host"""
+AWS_ACCESS_KEY_ID = AWS_ACCESS_KEY_ID
+AWS_SECRET_ACCESS_KEY = AWS_SECRET_ACCESS_KEY
+
+AWS_STORAGE_BUCKET_NAME = AWS_STORAGE_BUCKET_NAME 
+AWS_S3_CUSTOM_DOMAIN = AWS_S3_CUSTOM_DOMAIN
+
+AWS_LOCATION = 'static'
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+DEFAULT_FILE_STORAGE = 'AnkiForge.storage_backends.MediaStorage'
+
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Account Redirects
 
