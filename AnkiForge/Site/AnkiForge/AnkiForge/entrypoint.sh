b8480236abf9fe48a54e4071c5572deb665f98ed
Jordanw95 <65023872+Jordanw95@users.noreply.github.com>
2021-04-07 15:10:28 +0200

    Gunicorn

--- AnkiForge/Site/AnkiForge/AnkiForge/entrypoint.sh
+++ AnkiForge/Site/AnkiForge/AnkiForge/entrypoint.sh
@@ -5,6 +5,6 @@ python3.6 manage.py migrate
 # only needed on first launch of server
 # python3.6 manage.py loaddata StartUpFiles/DecksDump.json
 # python3.6 manage.py loaddata StartUpFiles/MembershipDump.json
-python3.6 manage.py runserver 0.0.0.0:8000
+gunicorn AnkiForge.wsgi:application --bind 0.0.0.0:8000
 
 exec "$@"
